f9de495ccd02d2ca90ad897c24a207a9b074e061
Jonas <familiehimmelberg@gmail.com>
2025-03-15 15:55:36 +0100

    Aktualisiere Datenbankverbindung mit neuem Host

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,6 @@ const DataBaseManager = require("./Database/DataBaseManager");
 const ExpressManager = require("./Express/ExpressManager");
 const SocketIOManager = require("./SocketIO/SocketIOManager");
 
-const databaseManager = new DataBaseManager("agione.ddns.de", "doublesnake");
+const databaseManager = new DataBaseManager("agione.v6.rocks", "doublesnake");
 const expressManger = new ExpressManager(databaseManager);
 const socketIOManager = new SocketIOManager(databaseManager, expressManger);
