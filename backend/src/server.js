4311b4c565e52b6add60450a69e5e514cb27c40f
Jonas <familiehimmelberg@gmail.com>
2025-03-17 22:03:32 +0100

    Aktualisiere Datenbankverbindung, um Umgebungsvariablen für den Host zu verwenden

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 const DataBaseManager = require("./Database/DataBaseManager");
 const ExpressManager = require("./Express/ExpressManager");
 const SocketIOManager = require("./SocketIO/SocketIOManager");
+require("dotenv").config();
 
-const databaseManager = new DataBaseManager("localhost", "doublesnake");
+const databaseManager = new DataBaseManager(process.env.DB_HOST || "localhost", "doublesnake");
 const expressManger = new ExpressManager(databaseManager);
 const socketIOManager = new SocketIOManager(databaseManager, expressManger);
