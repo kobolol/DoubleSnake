4fa345bf25e49355452e2c748c46899e30efa928
Jonas <77726472+kobolol@users.noreply.github.com>
2026-06-28 01:06:32 +0200

    Use environment variable for database name

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ const ExpressManager = require("./Express/ExpressManager");
 const SocketIOManager = require("./SocketIO/SocketIOManager");
 require("dotenv").config();
 
-const databaseManager = new DataBaseManager(process.env.DB_HOST || "localhost", "doublesnake");
+const databaseManager = new DataBaseManager(process.env.DB_HOST || "localhost", process.env.DB_DATABASE || "doublesnake");
 const expressManger = new ExpressManager(databaseManager);
 const socketIOManager = new SocketIOManager(databaseManager, expressManger);
